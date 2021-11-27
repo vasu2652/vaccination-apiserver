3e0d6376bc448faefa96fcd2d2811f088fded135
vasudev <palojuvasudev05@gmail.com>
2021-11-27 20:02:57 +0530

    Test1 to check the automatic deployment in heroku

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send("Welcome to Eduonix");
+  res.send("Welcome to Eduonix Vaccination Application");
 });
 
 module.exports = router;
